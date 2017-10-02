219d4796a68489e65433881c882d44572240df8a
Carlos Javier Urango Martinez <Carlos.X.Urango.-ND@disney.com>
2017-10-02 12:49:00 -0500

    adding api urls

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 axios.defaults.timeout = 5000;
-axios.defaults.baseURL = 'http://127.0.0.1:3000/api/v1';
+const api_urls = {
+  development: 'http://127.0.0.1:3000/api/v1',
+  production: 'https://pure-spire-62269.herokuapp.com/api/v1',
+}
+axios.defaults.baseURL = api_urls[process.env.NODE_ENV];
 
 export default axios;
